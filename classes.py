208020ced2d6bdfefdd5d4c249f256359a38fecd
Jelmer-ja <jelmer-ja@hotmail.com>
2017-12-12 13:15:39 +0100

    k

--- classes.py
+++ classes.py
@@ -13,53 +13,86 @@ class storydata:
     def __init__(self):
         self.stories = []
         self.ratings = []
+        self.urls = []
         self.roundedratings = []
-        self.urls = ['https://www.creepypasta.com/the-dead-zone/']
-        #self.getUrls()
-        self.saveStories()
+        self.collectStories()
+        self.plotRatings()
 
     #fill the list of urls with the creepypasta story lins from the text file
     def getUrls(self):
-        redflags = ['pastas-indexed-category','discussion-post','wp-content','category-','category/','update','page/','page-','tag/','author/']
-        blacklist = []
-        #Open the file of blacklisted pages, extract the names and close the file again
-        blfile = open('blacklist.txt','r')
-        for url in blfile.readlines():
-            blacklist.append(url[:-2])
-        blfile.close()
+        redflags = ['pastas-indexed-category','discussion-post','wp-content','category-','category/','update',
+                    'page/','page-','tag/','author/','creepypasta-','-creepypasta']
 
         #Open the file of website urls, filter out the ones that are not stories themselves
         #Add the rest to the list of urls
         urlfile = open('urls.txt','r')
         for url in urlfile.readlines():
-            url2 = url[:-2]
-            if(url2 not in blacklist):
-                clean = True
-                for flag in redflags:
-                    if(flag in url2):
-                        clean = False
-                if(clean):
-                    self.urls.append(url2)
+            clean = True
+            for flag in redflags:
+                if(flag in url):
+                    clean = False
+            if(clean):
+                self.urls.append(url[:-2])
         print(len(self.urls))
 
     def saveStories(self):
-        for url in self.urls:
+        for i in range(0,len(self.urls)):
+            url = self.urls[i]
+            print('Saving story ' + str(i) + ': ' + url)
             text,rating = self.getPage(url)
-            filename = '/home/jelmer/Documents/pastadata/' + str(self.urls.index(url)) + '.txt'
+            filename = '/home/jelmer/Documents/pastadata/' + str(i) + '.txt'
             f = open(filename,'w')
             f.write(str(rating))
             f.write('\n')
-            print(text)
             f.write(text)
             f.close()
 
+    # Returns the story from the url as plaintext and the rating as a float
+    def getPage(self, url):
+        # Retrieve the url and create a parser
+        scraper = cfs.create_scraper()
+        page = scraper.get(url).content
+        soup = BeautifulSoup(page, 'html.parser')
+        [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
+        readable_text = soup.getText().encode('utf-8','ignore')  # Extract the text from the html and convert it to ASCII
+
+
+        # Find and extract the rating
+        rb = soup.find('strong')
+        rating = float(rb.text.strip())  # strip() is used to remove starting and trailing
+
+        # Find and extract the story's text
+        count = False
+        text = ''
+        for s in readable_text.splitlines():
+            # print(s)
+            if not count:
+                if ('Add this post to your list of favorites' in s):
+                    count = True
+            else:
+                if ('Rate this item:' in s):
+                    break
+                else:
+                    text += s + '\n'
+
+        # Delete newlines at the end of the text so that they don't mess with the data later
+        for i in range(1, 20):
+            if text[-1] == '\n':
+                text = text[:-1]
+            else:
+                break
+        return text, rating
+
     #Retrieve every story and score for every url in the list and add them to the 'stories' and 'ratings' lists
-    def collectStories(self,x,y):
-        for i in range(x,math.max(y,len(self.urls))):
-            # GET TEXT AND RATING FROM FILE i
-            #self.stories.append(text)
-            #self.ratings.append(rating)
-            pass
+    def collectStories(self):
+        for i in range(0,826):
+            filename = '/home/jelmer/Documents/pastadata/' + str(i) + '.txt'
+            f = open(filename, 'r')
+            self.ratings.append(float(f.readlines()[0]))
+            #self.stories.append(file.read()[4:])
+            f.close()
+
+
 
     def roundRatings(self):
         avg = sum(self.ratings) / float(len(self.ratings))
@@ -88,39 +121,6 @@ class storydata:
         if isinstance(element, Comment):
             return False
         return True
-
-    #Returns the story from the url as plaintext and the rating as a float
-    def getPage(self,url):
-        #Retrieve the url and create a parser
-        scraper = cfs.create_scraper()
-        page = scraper.get(url).content
-        soup = BeautifulSoup(page, 'html.parser')
-        [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
-        readable_text = soup.getText().encode('utf-8', 'ignore') #Extract the text from the html and convert it to ASCII
-
-        #Find and extract the rating
-        rb = soup.find('strong')
-        rating = float(rb.text.strip())  # strip() is used to remove starting and trailing
-
-        #Find and extract the story's text
-        count = False
-        text = ''
-        for s in readable_text.splitlines():
-            if not count:
-                if('Add this post to your list of favorites' in s):
-                    count = True
-            else:
-                if('Rate this item:' in s):
-                    break
-                else:
-                    text += s + '\n'
-
-        #Delete newlines at the end of the text so that they don't mess with the data later
-        for i in range(1,12):
-            if text[-1] == '\n':
-                text = text[:-1]
-            else: break
-        return text, rating
 
     def getTrain(self):
         return None
